5264e656d2fba3a4f6505af67dcab5a805eb582d
Richard McDonald <rmcd@usgs.gov>
2021-06-28 09:56:52 -0600

    some recent changes

---
+++ nldi_xstool/ancillary.py
@@ -0,0 +1,186 @@
+'''
+    File name: ancillary.py
+    Author: Anders Hopkins, Richard McDonald
+'''
+
+import dataretrieval.nwis as nwis
+import requests
+from shapely.geometry import Point, LineString, Polygon
+
+# The following function converts NGVD29 to NAVD88 if gage is in NGVD29 using NOAA NGS Vertcon service api
+# https://www.ngs.noaa.gov/web_services/ncat/lat-long-height-service.shtml
+
+
+def getGageDatum(gagenum, verbose=False):
+    si = nwis.get_record(sites=gagenum, service='site')
+    if si['alt_datum_cd'].values[0] == 'NGVD29':
+        # print('conversion')
+        url = "https://www.ngs.noaa.gov/api/ncat/llh"
+        lat_str = 'lat_va'
+        lon_str = 'long_va'
+
+        alt_str = 'alt_va'
+        indatum_str = 'coord_datum_cd'
+        outdatum_str = 'NAD83(2011)'
+        inVertDataum_str = 'NGVD29'
+        outVertDataum = 'NAVD88'
+        if f'{si[indatum_str].values[0]}' == 'NAD83':
+            indatum = 'NAD83(2011)'
+        else:
+            indatum = f'{si[indatum_str].values[0]}'
+
+        ohgt = float(si[alt_str].values[0])*.3048
+
+        tmplonstr = si[lon_str].values[0]
+
+        if len(str(tmplonstr)) == 6:
+            tstr = '0'
+            tmplonstr = (tstr + str(tmplonstr))
+
+        payload = {
+            'lat': f'N{si[lat_str].values[0]}',
+            'lon': f'W{tmplonstr}',
+            'orthoHt': repr(ohgt),
+            'inDatum': indatum,
+            'outDatum': outdatum_str,
+            'inVertDatum': inVertDataum_str,
+            'outVertDatum': outVertDataum
+        }
+        r = requests.get(url, params=payload)
+        resp = r.json()
+        if verbose:
+            print(f'{si[indatum_str].values[0]}')
+            print(f'payload: {payload}')
+            print(resp)
+        return float(resp['destOrthoht'])
+    else:
+        # print('non-conversion')
+        return si['alt_va'].values[0]*.3048
+
+
+# Resolution types and their respective IDs for the Rest Service
+res_types = {'res_1m': 18, 'res_3m': 19, 'res_5m': 20, 'res_10m': 21, 'res_30m': 22, 'res_60m': 23}
+# res_types = {'res_1m': 1, 'res_3m': 2, 'res_5m': 3, 'res_10m': 4, 'res_30m': 5, 'res_60m': 6}
+dim_order = {'latlon': 0, 'lonlat': 1}
+# Create a bounding box from any geo type
+# 'width' is in meters. It is the width of the buffer to place around the input geometry
+
+
+def make_bbox(shape_type, coords, width):
+    if shape_type == 'point':
+        shape = Point(coords)
+
+    elif shape_type == 'line':
+        shape = LineString(coords)
+
+    elif shape_type == 'polygon':
+        shape = Polygon(coords)
+
+    converted_width = convert_width(width)
+    buff_shape = shape.buffer(converted_width)
+    return buff_shape.bounds
+
+# Convert the width of the buffer from meters to 'degree'
+# This is NOT a precise conversion, just a quick overestimation
+# Maybe fix this later
+
+
+def convert_width(width):
+    factor = 1/70000
+    dist = factor*width
+    return dist
+
+# Get request from arcgis Rest Services
+
+
+def get_dem(bbox, res_type):
+    minx = str(bbox[0])
+    miny = str(bbox[1])
+    maxx = str(bbox[2])
+    maxy = str(bbox[3])
+    res_id = res_types[res_type]
+
+    url = f'https://index.nationalmap.gov/arcgis/rest/services/3DEPElevationIndex/MapServer/{res_id}/query'
+    payload = {
+        # "where": "",
+        # "text": "",
+        # "objectIds": "",
+        # "time": "",
+        "geometry": "{xmin:\""+minx+"\",ymin:\""+miny+"\",xmax:\""+maxx+"\",ymax:\""+maxy+"\",spatialReference:{wkid:4326}}",
+        "geometryType": "esriGeometryEnvelope",
+        "inSR": "EPSG:4326",
+        "spatialRel": "esriSpatialRelIntersects",
+        # "relationParam": "",
+        # "outFields": "",
+        "returnGeometry": "true",
+        # "returnTrueCurves": "false",
+        "maxAllowableOffset": "100",
+        "geometryPrecision": "3",
+        "outSR": "EPSG:4326",
+        # "having": "",
+        # "returnIdsOnly": "false",
+        # "returnCountOnly": "false",
+        # "orderByFields": "",
+        # "groupByFieldsForStatistics": "",
+        # "outStatistics": "",
+        # "returnZ": "false",
+        # "returnM": "false",
+        # "gdbVersion": "",
+        # "historicMoment": "",
+        # "returnDistinctValues": "false",
+        # "resultOffset": "",
+        # "resultRecordCount": "",
+        # "queryByDistance": "",
+        # "returnExtentOnly": "false",
+        # "datumTransformation": "",
+        # "parameterValues": "",
+        # "rangeValues": "",
+        # "quantizationParameters": "",
+        "featureEncoding": "esriDefault",
+        "f": "geojson"
+        }
+
+    r = requests.get(url, params=payload)
+    resp = r.json()
+
+    # If the Rest Services has a 200 response
+    if 'features' in resp:
+        # If the features are not empty, then the DEM exist
+        if resp['features'] != []:
+            return True
+        # If features are empty, then no DEM
+        if resp['features'] == []:
+            return False
+    # If 'error', then there was an unsuccessful request
+    if 'error' in resp:
+        return 'Error!'
+
+# The function to loop thru all resolutions and submit queries
+
+
+def queryDEMs(shape_type, coords, width=100):
+    """
+    Queries 3DEP 3DEPElevationIndex and returns of dictionary of available
+    resolutions for shapes bounding box.
+
+    Args:
+        shape_type (Shapely geometric object in [Point, LineString, Polygon]): [description]
+        coords (List of tuples): For example, [(x,y), (x,y)]
+        width (int, optional): [width to buffer bounding box of shape]. Defaults to 100.
+    """
+    resp = {}  # Create an empty dictionary for the response
+    bbox = make_bbox(shape_type, coords, width)  # Make the bbox
+    print(bbox)
+    for res_type in res_types:   # Loop thru all resolutions and submit a query for each one
+        resp[res_type] = (get_dem(bbox, res_type))  # Add the response to the dictionary
+
+    # print(resp)
+    return(resp)
+
+
+def queryDEMsShape(bbox):
+    resp = {}
+
+    for res_type in res_types:
+        resp[res_type] = (get_dem(bbox, res_type))
+    return resp
